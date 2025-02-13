e25666c99745791ec0a9b2290581501804f2d0c9
AJ Javadi <amirjavadi25@gmail.com>
2025-02-13 01:11:21 -0600

    Remove unused index.php file

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * @Author: AJ  
- * @Email: someone@example.com
- * @Date: 2025-02-12 22:18:26 
- * @Last Modified by: Someone
- * @Last Modified time: 2025-02-13 00:26:00
- * @Description: file:///Users/aj/Herd/KBM/index.php
- */
